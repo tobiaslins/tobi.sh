4087d45cc3cc913e3a3b161bfc6bbfb378a8cc20
Tobias Lins <me@tobi.sh>
2020-04-01 12:52:54 +0200

    fix font

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
   content: ["./pages/**/*.tsx", "./components/**/*.tsx", "./styles/**/*.css"],
+  whitelist: ["html", "body"],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
