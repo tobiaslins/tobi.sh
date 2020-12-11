1d2ccb0416b3034c8105891cc8d82305cc88dfbc
Tobias Lins <me@tobi.sh>
2020-12-11 16:03:19 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,21 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class DefaultDocument extends Document {
-    public render() {
-        return (
-            <Html lang="en">
-                <Head />
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  public render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "70eb6fd2c3fc491fbb0196db6b00ab55"}'
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
