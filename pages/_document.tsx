a5871af28ad708ac694101df386ad8c5774e166f
Tobias Lins <me@tobi.sh>
2021-12-09 16:09:45 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,6 @@ export default class DefaultDocument extends Document {
   public render() {
     return (
       <Html lang="en">
-        <Head>
-          <script
-            defer
-            src="https://static.cloudflareinsights.com/beacon.min.js"
-            data-cf-beacon='{"token": "70eb6fd2c3fc491fbb0196db6b00ab55"}'
-          ></script>
-        </Head>
         <body>
           <Main />
           <NextScript />
